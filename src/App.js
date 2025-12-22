80ae8d13501002c9bce28f867e3f0f0ee2b3b834
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-12-22 12:29:09 +0000

    Created Basic Search Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-
 import './App.css';
+import Dictionary from './Dictionary';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
-         <h1>hello</h1>
+         <h1>Dictionary App</h1>
       </header>
+      <main>
+        <Dictionary  />
+      </main>
+      <footer>
+        <span>This Project was coded by Jade coster </span>
+      </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
