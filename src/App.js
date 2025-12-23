9aa4388c3a18d46e64f82bca953a01ded5cf3326
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-12-23 12:01:46 +0000

    Completed Search engine

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ export default function App() {
         <Dictionary  />
       </main>
       <footer>
-        <span>This Project was coded by Jade coster </span>
+        <span>This Project was coded by <strong>Jade coster</strong>. It is open-sourced on </span>
+          <span>
+         <a href='https://github.com/Vadia-Jade/dictionary-app-react' target='_blank' rel='noreferrer'>GitHub</a>
+         </span>
+         <span>  and Hosted on </span>
+         <span>
+          <a href='https://thunderous-kangaroo-a1e322.netlify.app/' target='_blank' rel='noreferrer'>Netlify</a>
+        </span>
       </footer>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react"
+import Results from "./Results";
 import axios from 'axios';
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
-    function handleResponse(response){
-        console.log(response.data[0]);
+    function handleResponse(response) {
+       setResults(response.data[0]);
     }
 
     function search(event){
         event.preventDefault();
-    
-        let apiKey = "034407t747af2dfd1c31bo02c7fc4156";
-        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-        
+       
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+
         axios.get(apiUrl).then(handleResponse);
 
     }
@@ -26,6 +27,7 @@ export default function Dictionary(){
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
             </form>
+            <Results results={results} />
         </div>
     );
 }
