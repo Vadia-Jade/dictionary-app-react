24c612f8c3307bd6eb27ac2b44f3287e44099920
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-12-28 11:36:23 +0000

    Added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from 'axios';
 import "./Dictionary.css";
 
@@ -7,14 +8,23 @@ export default function Dictionary(props){
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState([]);
 
     function handleResponse(response) {
        setResults(response.data[0]);
     }
 
+    function handlePhotosResponse(response){
+        setPhotos(response.data.photos);
+    }
+
     function search () {
           let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let photosApiKey ="034407t747af2dfd1c31bo02c7fc4156";
+        let photosApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photosApiKey}`;
+        axios.get(photosApiUrl).then(handlePhotosResponse);
     }
 
     function handleSubmit(event){
@@ -44,6 +54,7 @@ export default function Dictionary(props){
             </div>
             </section>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     );
      }else {
---
+++ src/Photos.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Photos(props){
+  if (props.photos.length) {
+    return(
+         <section className="Photos">
+            <div className="row">
+                {props.photos.map(function(photo, index){
+                    return(
+                        <div className="col-4" key={index}>
+                            <a href={photo.src.original}
+                            target="_blabk" rel="noopener noreferrer">
+                                <img src={photo.src.landscape} className="img-fluid" alt={photo.photographer}/>
+                            </a>
+                        </div>
+
+                    );
+                })}
+
+            </div>
+
+         </section>
+    );
+  }
+    return null;
+}
